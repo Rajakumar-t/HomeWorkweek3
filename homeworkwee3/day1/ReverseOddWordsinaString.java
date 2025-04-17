80c376dc17c877e93bb34789e7bb5d6a9ba22d3a
Rajakumar-t <rajakumart@gmail.com>
2025-04-17 10:44:44 +0100

    Update ReverseOddWordsinaString.java

--- homeworkwee3/day1/ReverseOddWordsinaString.java
+++ homeworkwee3/day1/ReverseOddWordsinaString.java
@@ -8,11 +8,16 @@ public class ReverseOddWordsinaString {
 
 	public static void main(String[] args) {
 		String test = "I am a software tester";
-		String [] word = test.split(" ");
-		System.out.println("split"+Arrays.toString(word));
-		for (int i=0;i<word.length;i++) {
-			
+		String[] stest = test.split(" ");
+		for (int i=0;i<stest.length;i++) {
+			if(i%2==1) {
+			String outcome = " ";
+		 for (int j=stest[i].length()-1;j>=0;j--) {
+			 outcome += stest[i].charAt(j);
+		 }System.out.print(outcome + " ");
+		}	else {
+			System.out.print(stest[i]);}		
 		}
-	}
-
+		}
+	
 }
